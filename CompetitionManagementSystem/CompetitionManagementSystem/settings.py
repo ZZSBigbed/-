2322aa55507179ab24f60fb13e85c578c4d20327
201792417 <34784185+Riverlly@users.noreply.github.com>
2020-02-17 12:03:20 +0800

    基本煤改什么...

--- CompetitionManagementSystem/CompetitionManagementSystem/settings.py
+++ CompetitionManagementSystem/CompetitionManagementSystem/settings.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 SECRET_KEY = 's*y6ocy1-5&k34uomgfrfr2arhp9gy&wg3%5x2&90=bb!k=%*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -138,10 +138,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR,'static'),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 EMAIL_HOST = "smtp.sina.cn"
 EMAIL_PORT = 25
